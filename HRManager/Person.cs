4d66ca34a1a34398a68c2cc1d1010bc51c721d3c
Emmanuel Durin <emmanueldurin@gmail.com>
2017-02-28 16:31:10 +0100

    SharedSizeGroup pour les grids

---
+++ HRManager/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel;
+
+namespace HRManager
+{
+  // BindableBase Permet d'émettre des évts lors des modifs des propriétés
+
+  public class Person : BindableBase
+  {
+    private int age;
+    public int Age
+    {
+      get { return age; }
+      set
+      {
+        SetProperty(ref age, value);
+      }
+    }
+    private string firstname;
+
+    public string Firstname
+    {
+      get { return firstname; }
+      set
+      {
+        SetProperty(ref firstname, value);
+      }
+    }
+    private string lastname;
+    public string Lastname
+    {
+      get { return lastname; }
+      set
+      {
+        SetProperty(ref lastname, value);
+      }
+    }
+
+
+
+
+  }
+}
---
+++ HRManager/RelayCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ViewModel.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        #region Fields
+
+        private Action<object> _execute;
+        public Action<object> Action
+        {
+            get { return _execute; }
+            set { _execute = value; }
+        }
+        readonly Predicate<object> _canExecute;
+
+        #endregion // Fields
+
+        #region Constructors
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        #endregion // Constructors
+
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+        public event EventHandler CanExecuteChanged;
+        public void FireExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+                CanExecuteChanged(this, new EventArgs() );
+        }
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+        #endregion // ICommand Members
+    }
+}
